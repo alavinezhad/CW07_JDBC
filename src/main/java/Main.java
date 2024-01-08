5a8c7cf301c8d011313206333051f7afd8b86c4f
Hassan Alavinezhad <j.hassan.alavi@gmail.com>
2024-01-08 11:43:54 +0330

    check all methods

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,20 @@
+import entities.User;
+import service.UserService;
+
+import java.sql.SQLException;
+
 public class Main {
+    public static void main(String[] args) throws SQLException {
+        UserService userService = new UserService();
+
+        userService.addUser();
+        User foundUser = userService.findUserById(2);
+        System.out.println(foundUser);
+
+        User user1 = userService.findUserById(4);
+        userService.updateUser(user1);
+        User user8 = userService.findUserById(8);
+        userService.deleteUser(user8);
+        userService.loadAllUsers();
+    }
 }
