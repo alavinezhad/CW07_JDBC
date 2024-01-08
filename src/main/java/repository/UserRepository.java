f5a4b6b88b201c629b98885460d3dcf10acd0cbf
Hassan Alavinezhad <j.hassan.alavi@gmail.com>
2024-01-08 08:54:45 +0330

    add save method

--- src/main/java/repository/UserRepository.java
+++ src/main/java/repository/UserRepository.java
@@ -1,4 +1,28 @@
 package repository;
 
+import entities.User;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class UserRepository {
+    private final JdbcConnection jdbcConnection = new JdbcConnection();
+    private final Connection connection = jdbcConnection.getConnection();
+
+    public UserRepository() throws SQLException {
+    }
+
+    public int save(User user) throws SQLException {
+        String addUser = "INSERT INTO \"user\" (username, password, signup_date)\n" +
+                "VALUES (?, ?, now());";
+        PreparedStatement preparedStatement = connection.prepareStatement(addUser,
+                Statement.RETURN_GENERATED_KEYS);
+        preparedStatement.setString(1, user.getUsername());
+        preparedStatement.setString(2, user.getPassword());
+
+        return preparedStatement.executeUpdate();
+    }
+
 }
