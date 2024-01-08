d0e3476615f4aedfc75bf9e8d4ba1c6db7403604
Hassan Alavinezhad <j.hassan.alavi@gmail.com>
2024-01-08 08:07:02 +0330

    add no arg and all arg constructor and a constructor with all fields except user_id

--- src/main/java/entities/User.java
+++ src/main/java/entities/User.java
@@ -1,4 +1,26 @@
 package entities;
 
+import java.util.Date;
+
 public class User {
+    private int user_id;
+    private String username;
+    private String password;
+    private Date signupDate;
+
+    public User() {
+    }
+
+    public User(String username, String password, Date signupDate) {
+        this.username = username;
+        this.password = password;
+        this.signupDate = signupDate;
+    }
+
+    public User(int user_id, String username, String password, Date signupDate) {
+        this.user_id = user_id;
+        this.username = username;
+        this.password = password;
+        this.signupDate = signupDate;
+    }
 }
