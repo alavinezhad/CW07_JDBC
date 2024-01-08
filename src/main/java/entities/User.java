3b73c0ecc85c0516b5569c658c3162444d44d0e1
Hassan Alavinezhad <j.hassan.alavi@gmail.com>
2024-01-08 08:07:35 +0330

    add getter setter

--- src/main/java/entities/User.java
+++ src/main/java/entities/User.java
@@ -23,4 +23,36 @@ public class User {
         this.password = password;
         this.signupDate = signupDate;
     }
+
+    public int getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Date getSignupDate() {
+        return signupDate;
+    }
+
+    public void setSignupDate(Date signupDate) {
+        this.signupDate = signupDate;
+    }
 }
